74da40a6809b570e045ffd4eb5bf2097ba0c4928
AMCodeBytes <57409463+AMCodeBytes@users.noreply.github.com>
2024-03-27 21:18:28 +0000

    Created generate uuid v4 function

---
+++ utilities/uuid.go
@@ -0,0 +1,17 @@
+package utilities
+
+import (
+	"errors"
+
+	"github.com/google/uuid"
+)
+
+func GenerateUUID() (string, error) {
+	uuid, err := uuid.NewRandom()
+
+	if err != nil {
+		return "", errors.New("failed to generate uuid")
+	}
+
+	return uuid.String(), nil
+}
