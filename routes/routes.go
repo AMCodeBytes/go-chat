df029bfdabd2b826b8d50d6b128298f9bdbbc89f
AMCodeBytes <57409463+AMCodeBytes@users.noreply.github.com>
2024-03-29 23:30:38 +0000

    Created the basics for the sign up page

--- routes/routes.go
+++ routes/routes.go
@@ -11,11 +11,20 @@ func RegisterRoutes(server *gin.Engine) {
 	// Home
 	server.GET("/", home)
 
+	// Sign up
+	server.GET("/register", register)
+
 	server.GET("/ws", handlers.Chat)
 }
 
 func home(context *gin.Context) {
 	context.HTML(http.StatusOK, "index.html", gin.H{
 		"content": "You have reached the homepage of the chat application...",
+	})
+}
+
+func register(context *gin.Context) {
+	context.HTML(http.StatusOK, "register.html", gin.H{
+		"content": "Please sign up to be able to chat.",
 	})
 }
