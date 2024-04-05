338ed840932303dc2e6830fbebb5199d892d2b37
AMCodeBytes <57409463+AMCodeBytes@users.noreply.github.com>
2024-04-05 21:38:46 +0100

    Created updates for the WS chat back-end

--- handlers/chat.go
+++ handlers/chat.go
@@ -2,28 +2,33 @@ package handlers
 
 import (
 	"fmt"
+	"log"
 	"net/http"
-	"time"
+	"sort"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
 )
+
+var wsChan = make(chan WsPayload)
+var clients = make(map[WsConnection]string)
 
 type WsConnection struct {
 	*websocket.Conn
 }
 
 type WsResponse struct {
-	Action      string `json:"action"`
-	Message     string `json:"message"`
-	MessageType string `json:"message_type"`
+	Action         string   `json:"action"`
+	Message        string   `json:"message"`
+	MessageType    string   `json:"message_type"`
+	ConnectedUsers []string `json:"connected_users"`
 }
 
 type WsPayload struct {
 	Action   string       `json:"action"`
 	Username string       `json:"username"`
 	Message  string       `json:"message"`
-	Conn     WsConnection `json:"_"`
+	Conn     WsConnection `json:"-"`
 }
 
 var upgrader = websocket.Upgrader{
@@ -32,7 +37,7 @@ var upgrader = websocket.Upgrader{
 }
 
 func Chat(context *gin.Context) {
-	conn, err := upgrader.Upgrade(context.Writer, context.Request, nil)
+	ws, err := upgrader.Upgrade(context.Writer, context.Request, nil)
 
 	if err != nil {
 		fmt.Println(err)
@@ -40,10 +45,82 @@ func Chat(context *gin.Context) {
 		return
 	}
 
-	defer conn.Close()
+	defer ws.Close()
+
+	conn := WsConnection{Conn: ws}
+	clients[conn] = ""
+
+	// for {
+	// 	conn.WriteMessage(websocket.TextMessage, []byte("Hello world! This is a websocket connection"))
+	// 	time.Sleep(time.Second)
+	// }
+
+	go ListenForWs(&conn)
+}
+
+func ListenForWs(conn *WsConnection) {
+	var payload WsPayload
 
 	for {
-		conn.WriteMessage(websocket.TextMessage, []byte("Hello world! This is a websocket connection"))
-		time.Sleep(time.Second)
+		err := conn.ReadJSON(&payload)
+		if err != nil {
+			// Do nothing
+		} else {
+			payload.Conn = *conn
+			wsChan <- payload
+		}
 	}
+}
+
+func ListenToWsChannel() {
+	var response WsResponse
+
+	for {
+		e := <-wsChan
+
+		switch e.Action {
+		case "username":
+			// List of all users and send via broadcast
+		case "left":
+			// handle user leaving
+			response.Action = "list_users"
+
+			delete(clients, e.Conn)
+
+			users := getUserList()
+			response.ConnectedUsers = users
+
+			broadcastToAll(response)
+		case "broadcast":
+			response.Action = "broadcast"
+			response.Message = fmt.Sprintf("<strong>%s</strong>: %s", e.Username, e.Message)
+
+			broadcastToAll(response)
+		}
+	}
+}
+
+func broadcastToAll(response WsResponse) {
+	for client := range clients {
+		err := client.WriteJSON(response)
+
+		if err != nil {
+			log.Println("websocket err")
+			_ = client.Close()
+			delete(clients, client)
+		}
+	}
+}
+
+func getUserList() []string {
+	var userList []string
+
+	for _, x := range clients {
+		if x != "" {
+			userList = append(userList, x)
+		}
+	}
+
+	sort.Strings(userList)
+	return userList
 }
